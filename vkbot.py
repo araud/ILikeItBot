0f73a957ddcae00d67acb79cc76d26f4f29e49a5
Raud <alexander.a.raud@intel.com>
2016-09-12 22:36:48 +0300

    likes limits

--- vkbot.py
+++ vkbot.py
@@ -23,15 +23,15 @@ class ILikeItBot:
         self.api = vk.API(session, v='5.53', lang='ru', timeout=10)  # 5.35
 
     def online_users_from_group(self, group_id):
-        offset = 0
-        while True:
-            fields = 'id,blacklisted,is_friend,can_send_friend_request,followers_count'
-            users = self.api.users.search(count=1000, online=1, age_from=20, fields=fields, group_id=group_id)
-            offset += len(users['items'])
-            for user in users['items']:
-                yield user
-            if offset > users['count']:
-                break
+        fields = 'id,blacklisted,is_friend,can_send_friend_request,followers_count'
+        age_step = 1
+        for age in range(20, 60, age_step):
+            for sex in [1, 2]:
+                users = self.api.users.search(
+                    count=1000, online=1, age_from=age, age_to=age+age_step, fields=fields, group_id=group_id, sex=sex
+                )
+                for user in users['items']:
+                    yield user
 
     def get_avatar(self, user_id):
         return self.api.photos.get(owner_id=user_id, album_id='profile')
@@ -79,13 +79,16 @@ def main():
 
     white_list = read_list(args.whitelist)
     black_list = read_list(args.blacklist)
-
+    likes_limit = 499
+    sleep_seconds = 7
+    count = 0
     with open(args.blacklist, 'a') as nbl:
         bot = ILikeItBot(args.app, args.user, args.password)
         for user in bot.online_users_from_group(args.group):
             if (user['id'] not in white_list) or (user['id'] in black_list):
                 continue
-            print "vk.com/id%d" % user['id']
+            count += 1
+            print "vk.com/id%d:\t%d" % (user['id'], count)
             posts = bot.get_own_posts(user['id'])
             nbl.write('%d\n' % user['id'])
 
@@ -95,7 +98,7 @@ def main():
                 text = item['text'].decode('utf-8').lower()
                 if 'engl' in text or 'англ' in text:
                     print bot.repost(item, args.repost)
-                    time.sleep(5)
+                    time.sleep(sleep_seconds)
                     break
             recent_posts = sorted(
                 [item for item in posts['items'] if item['likes']['can_like']],
@@ -105,7 +108,8 @@ def main():
             if recent_posts:
                 recent_post = recent_posts[0]
                 print bot.like_post(recent_post['owner_id'], recent_post['id'])
-                time.sleep(5)
+                likes_limit -= 1
+                time.sleep(sleep_seconds)
             else:
                 print "No posts to like"
 
@@ -118,7 +122,8 @@ def main():
             if avatars:
                 avatar = avatars[0]
                 print bot.like_post(avatar['owner_id'], avatar['post_id'])
-                time.sleep(5)
+                likes_limit -= 1
+                time.sleep(sleep_seconds)
             else:
                 print "No avatars to like"
             """
@@ -126,6 +131,9 @@ def main():
             if 300 < counters['friends'] < 700:
                 bot.invite_to_group(user['id'], args.invite)
             """
+            if likes_limit <= 0:
+                print "Likes limit reached..."
+                break
 
 if __name__ == '__main__':
     main()
